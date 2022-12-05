d05b50d5fd07a7d71574d77887a769854b5ad2ab
Jeethendra Balaji <jeeboxical@gmail.com>
2022-12-05 19:03:25 +0530

    adds border, adds event listener on grid elements, change color on mousedown

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let size = 16;
 const gridContainer = document.querySelector('.grid-container');
 const scale = document.getElementById("scale");
+
+// neat trick from michalosman
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function setupGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -9,6 +13,8 @@ function setupGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mousedown', changeColor)
+        gridElement.addEventListener('mouseover', changeColor)
         gridContainer.appendChild(gridElement)
     }
 }
@@ -17,4 +23,14 @@ scale.onchange = function() {
     const size = this.value;
     gridContainer.innerHTML = '';
     setupGrid(size);
+}
+
+function changeColor(e) {
+    if (e.type == "mouseover" && !mouseDown) return;
+    const gridElement = e.target;
+    gridElement.style.backgroundColor = "black";
+}
+
+window.onload = () => {
+    setupGrid(scale.value)
 }

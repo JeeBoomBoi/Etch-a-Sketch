d7ad8a8c0dd5c3bcb84eebc371c3db238c0c639a
Jeethendra Balaji <jeeboxical@gmail.com>
2022-12-05 18:09:34 +0530

    adds a slider scale for grid size input

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let size = 16;
 const gridContainer = document.querySelector('.grid-container');
+const scale = document.getElementById("scale");
 
-function setupGrid() {
+function setupGrid(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
@@ -12,4 +13,8 @@ function setupGrid() {
     }
 }
 
-setupGrid();
+scale.onchange = function() {
+    const size = this.value;
+    gridContainer.innerHTML = '';
+    setupGrid(size);
+}
